57d96345cc7826f238803587e6f27a7587759f32
Masynchin <banzur.badurov@gmail.com>
2020-11-29 17:45:45 +0300

    test floor generation

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
+import os
+from random import choice
+
 import pygame
 
-pygame.init()
-pygame.display.set_caption('game')
-screen = pygame.display.set_mode((600, 400))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class Floor(pygame.sprite.Sprite):
+	def __init__(self, width, height, floor_type: str):
+		super().__init__()
+		self.width = width
+		self.height = height
+		self.type = floor_type
 
-    screen.fill('white')
-    pygame.display.flip()
+		self.image = self._generate_image()
+		self.rect = self.image.get_rect()
 
-pygame.quit()
+	def _generate_image(self):
+		plate_size = get_image_size(self._get_random_plate())
+		plate_width, plate_height = plate_size
+
+		image_width = self.width * plate_width
+		image_height = self.height * plate_height
+
+		image = pygame.Surface((image_width, image_height))
+		for i in range(self.width):
+			for j in range(self.height):
+				plate = pygame.image.load(self._get_random_plate())
+				image.blit(plate, (plate_width * i, plate_height * j))
+
+		return image
+
+	def _get_random_plate(self):
+		return choice(self._get_plates())
+
+	def _get_plates(self):
+		plates = [f'source/floor/{plate_filename}' for plate_filename in os.listdir('source/floor')
+				   if self.type in plate_filename]
+		return plates
+
+
+def get_image_size(path):
+	from PIL import Image
+	image = Image.open(path)
+	return image.size
+
+
+def main():
+	w, h = 600, 400
+	screen = pygame.display.set_mode((w, h))
+	pygame.display.set_caption('Floor')
+
+	floor = Floor(4, 3, 'dungeon')
+
+	running = True
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		screen.fill((0, 0, 0))
+		screen.blit(floor.image, floor.rect)
+		pygame.display.flip()
+
+
+if __name__ == '__main__':
+	pygame.init()
+	main()
+	pygame.quit()
