15ab4d40515df30e7f72e4f9a89cb4c8591a1a40
lyceum-max-smirnov <lera.fedotovaaa@gmail.com>
2020-12-02 00:25:23 +0300

    use refactored MenuButton, uncomment test_hero, style improve

--- main.py
+++ main.py
@@ -1,102 +1,45 @@
 import os
 import pygame
 
-from classes import Floor, Knight, MenuBtn
+from classes import Floor, Knight, MenuButton
 import helpers
 
 from config import FPS
 
 
-# def main():
-# 	screen_size = screen_width, screen_height = 600, 400
-# 	# screen = pygame.display.set_mode(screen_size)
-# 	screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
-# 	pygame.display.set_caption('Hero movement')
-# 	clock = pygame.time.Clock()
-
-# 	floor = Floor(24, 12, 'dungeon')
-# 	knight = Knight()
-
-# 	running = True
-# 	while running:
-
-# 		for event in pygame.event.get():
-# 			if (event.type == pygame.QUIT or
-# 					event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-# 				running = False
-
-# 			elif event.type == pygame.KEYDOWN:
-# 				if event.key in helpers.MOVEMENT_KEYS:
-# 					x_shift, y_shift = handle_movement(event)
-# 					knight.change_direction(x_shift, y_shift)
-
-# 			elif event.type == pygame.KEYUP:
-# 				if event.key in helpers.MOVEMENT_KEYS:
-# 					x_shift, y_shift = handle_movement(event)
-# 					knight.change_direction(-x_shift, -y_shift)
-
-# 		knight.move()
-
-# 		screen.fill((0, 0, 0))
-
-# 		screen.blit(floor.image, floor.rect)
-# 		screen.blit(knight.image, knight.rect)
-
-# 		pygame.display.flip()
-# 		clock.tick(FPS)
-
-
-def main():
+def test_hero():
 	screen_size = screen_width, screen_height = 600, 400
 	screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
-	pygame.display.set_caption('Hero movement')
+	pygame.display.set_caption('Hero Test')
 	clock = pygame.time.Clock()
-	running = True
 
+	floor = Floor(24, 12, 'dungeon')
+	knight = Knight()
+
+	running = True
 	while running:
-		btn_continue = pygame.image.load(f'source/buttons/new_game.png')
-		btn_new_game = pygame.image.load(f'source/buttons/icon_continue.png')
-		btn_settings = pygame.image.load(f'source/buttons/icon_settings.png')
-		btn_exit = pygame.image.load(f'source/buttons/icon_exit.png')
-		btn_settings = pygame.transform.scale(btn_settings, (60, 60))
-		btn_exit = pygame.transform.scale(btn_exit, (60, 60))
-		btn_rect_continue = pygame.draw.rect(
-			screen, "red", (400, 650, 528, 714))
-		btn_rect_new_game = pygame.draw.rect(
-			screen, "red", (800, 650, 928, 714))
-		btn_rect_settings = pygame.draw.rect(
-			screen, "red", (600, 650, 664, 714))
-		btn_rect_exit = pygame.draw.rect(
-			screen, "red", (200, 650, 264, 714))
+
 		for event in pygame.event.get():
 			if (event.type == pygame.QUIT or
 					event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
 				running = False
 
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if btn_rect_continue.collidepoint(event.pos):
-					print('continue')
+			elif event.type == pygame.KEYDOWN:
+				if event.key in helpers.MOVEMENT_KEYS:
+					x_shift, y_shift = handle_movement(event)
+					knight.change_direction(x_shift, y_shift)
 
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if btn_rect_new_game.collidepoint(event.pos):
-					print('new_game')
+			elif event.type == pygame.KEYUP:
+				if event.key in helpers.MOVEMENT_KEYS:
+					x_shift, y_shift = handle_movement(event)
+					knight.change_direction(-x_shift, -y_shift)
 
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if btn_rect_settings.collidepoint(event.pos):
-					print('settings')
+		knight.move()
 
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				if btn_rect_exit.collidepoint(event.pos):
-					running = False
+		screen.fill((0, 0, 0))
 
-		screen.fill((123, 123, 123))
-		screen.blit(btn_continue, (400, 650))
-		screen.blit(btn_new_game, (800, 650))
-		screen.blit(btn_settings, (630, 655))
-		screen.blit(btn_exit, (200, 655))
-
-		# screen.blit(floor.image, floor.rect)
-		# screen.blit(knight.image, knight.rect)
+		screen.blit(floor.image, floor.rect)
+		screen.blit(knight.image, knight.rect)
 
 		pygame.display.flip()
 		clock.tick(FPS)
@@ -114,7 +57,50 @@ def handle_movement(event):
 	return x_shift, y_shift
 
 
+def test_menu():
+	screen_size = screen_width, screen_height = 600, 400
+	screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+	pygame.display.set_caption('Menu Test')
+	clock = pygame.time.Clock()
+
+	new_game_button = MenuButton('new_game', x=100, y=100)
+	continue_button = MenuButton('continue', x=200, y=200)
+	home_button     = MenuButton('home',     x=300, y=300)
+	settings_button = MenuButton('settings', x=400, y=400)
+	exit_button     = MenuButton('exit',     x=500, y=500)
+
+	running = True
+	while running:
+		for event in pygame.event.get():
+			if (event.type == pygame.QUIT or
+					event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+				running = False
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				if continue_button.collidepoint(event.pos):
+					print('continue')
+				elif new_game_button.collidepoint(event.pos):
+					print('new_game')
+				elif home_button.collidepoint(event.pos):
+					print('home')
+				elif settings_button.collidepoint(event.pos):
+					print('settings')
+				elif exit_button.collidepoint(event.pos):
+					print('exit')
+					running = False
+
+		screen.fill((123, 123, 123))
+
+		screen.blit(new_game_button.image, new_game_button.rect)
+		screen.blit(continue_button.image, continue_button.rect)
+		screen.blit(home_button.image, home_button.rect)
+		screen.blit(settings_button.image, settings_button.rect)
+		screen.blit(exit_button.image, exit_button.rect)
+
+		pygame.display.flip()
+		clock.tick(FPS)
+
+
 if __name__ == '__main__':
 	pygame.init()
-	main()
+	test_menu()
 	pygame.quit()
