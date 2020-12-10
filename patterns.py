cb3bc2131e14aeac13bf38c23e0adafeb6315730
lyceum-max-smirnov <lera.fedotovaaa@gmail.com>
2020-12-10 16:32:23 +0300

    move pattern to patterns.py

---
+++ patterns.py
@@ -0,0 +1,15 @@
+def get_random_pattern():
+	return pattern
+
+	
+pattern = ('           WWWWW',
+		   '               W',
+		   '               W',
+		   '               W',
+		   '               G',
+		   '               G',
+		   '               G',
+		   'W              G',
+		   'W   W      W   W',
+		   'W              W',
+		   'WWWWGGGGGGGGWWWW',)
