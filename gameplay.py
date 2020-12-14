92a57c5e4cec684e3bbff34b7afa0370ec1e01ab
lyceum-max-smirnov <75032355+lyceum-max-smirnov@users.noreply.github.com>
2020-12-14 18:00:22 +0300

    Strange git commit

--- gameplay.py
+++ gameplay.py
@@ -9,13 +9,8 @@ import helpers
 def play_level(screen, cursor, hero, location='dungeon'):
     clock = pygame.time.Clock()
 
-    bullets = pygame.sprite.Group()
-
-<<<<<<< HEAD
     room = Level(location).current_room
-=======
     room = Room(get_random_pattern(), location)
->>>>>>> e41ad689231e2ca5e1dcb4d761f27b4afcd40da7
     hero.rect.center = room.hero_position
 
     enemies = helpers.RectGroup()
@@ -23,6 +18,8 @@ def play_level(screen, cursor, hero, location='dungeon'):
         enemy = Dardo()
         enemy.rect.center = enemy_spawnpoint
         enemies.add(enemy)
+    
+    bullets = pygame.sprite.Group()
 
     mouse_pressed = False
     running = True
